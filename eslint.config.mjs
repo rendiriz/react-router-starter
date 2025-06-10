57bbf7e5e0827bb6a30bd892fa8dcd27fe288549
Rendi Rizki <rendirizkir@gmail.com>
2025-06-10 10:49:00 +0700

    chore: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+// eslint.config.js
+import js from '@eslint/js';
+// flat‐config entry point for prettier
+import prettier from 'eslint-config-prettier/flat';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import onlyWarn from 'eslint-plugin-only-warn';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  // the built-in “recommended” JS config
+  js.configs.recommended,
+
+  // the flat version of eslint-config-prettier
+  prettier,
+
+  // typescript-eslint’s flat “recommended”
+  ...tseslint.configs.recommended,
+
+  // turn everything into warnings
+  {
+    plugins: { onlyWarn },
+  },
+
+  // react
+  {
+    ...pluginReact.configs.flat.recommended,
+    languageOptions: {
+      ...pluginReact.configs.flat.recommended.languageOptions,
+      globals: {
+        ...pluginReact.configs.flat.recommended.languageOptions.globals,
+        ...globals.serviceworker,
+      },
+    },
+  },
+
+  // react-hooks
+  {
+    plugins: { 'react-hooks': pluginReactHooks },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off', // new JSX transform
+    },
+  },
+
+  // jsx-a11y (flat config support added in v7+)
+  {
+    plugins: { 'jsx-a11y': pluginJsxA11y },
+    rules: pluginJsxA11y.configs.recommended.rules,
+  },
+
+  // ignores
+  {
+    ignores: ['dist/**', 'build/**', 'node_modules/**'],
+  },
+];
