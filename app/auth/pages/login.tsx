934ac39c2952f4f78331d82b4b31d04219bffd9b
Rendi Rizki <rendirizkir@gmail.com>
2025-06-10 13:15:20 +0700

    chore: add shadcn

--- app/auth/pages/login.tsx
+++ app/auth/pages/login.tsx
@@ -1,6 +1,11 @@
 export default function Login() {
   return (
     <div>
+      <title>Login</title>
+      <meta
+        name="description"
+        content="Login Description"
+      />
       <h1>Login Page</h1>
     </div>
   );
--- app/public/pages/home.tsx
+++ app/public/pages/home.tsx
@@ -1,6 +1,11 @@
 export default function Home() {
   return (
     <div>
+      <title>Home</title>
+      <meta
+        name="description"
+        content="Home Description"
+      />
       <h1>Home Page</h1>
     </div>
   );
